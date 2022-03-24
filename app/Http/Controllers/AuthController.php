599cdf2038af0d84fa8df5c2d1c910f6b9778003
alaaahmeedd <alaa.aahmeedd@gmail.com>
2022-03-24 17:23:17 +0200

    add token per user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
-use Dirape\Token\Token;
-use Laravel\Sanctum\PersonalAccessToken;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cookie;
-use Symfony\Component\HttpFoundation\Response;
 
 
 
@@ -22,28 +17,23 @@ class AuthController extends Controller
 
             'name' => 'required|string',
             'email' => 'regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
-            // 'email' => 'required|email|unique:users,email',
             'password' => 'required|string',
             'phone' => 'required|string',
-            // 'token_key' => 'string'
-
+   
         ]);
         
-
         $user = User::create([
             'name' => $fields['name'],
             'email' => $fields['email'],
             'password' => bcrypt($fields['password']),
             'phone' => $fields['phone'],
             
-
-      
         ]);
         
-     
         $token = $user->createToken('myapptoken')->plainTextToken;
+        $token= substr($token , -40,40);
         User::where('id', $user->id)->update(['api_token' => $token]);
-        PersonalAccessToken::where('id',$user->id)->update(['token'=>$token]);
+    
 
         $response = [
             'user' => $user,
@@ -51,37 +41,22 @@ class AuthController extends Controller
         ];
 
         return response($response, 201);
-
-
-        // return response([$user], 201);
+        
     }
     
     public function login(Request $request){
         
         $fields = $request->validate([
-            
             'email' => 'regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
-            // 'email' =>'required|email',
             'password' =>'required|string',
-            // 'password' =>'required|string|confirmed'
         ]);
 
         // Check email and Check password
-        // $user = User::where('email', $fields['email'])->first();
+        
         $user = User::where('email' , $fields ['email'])->first();
 
-        $set = PersonalAccessToken::where('tokenable_id', $user->id)->first();
-
-        // if($user->api_token == $set->)
-        
         $order = Order::select('End_time')->where('user_id',$user->id)->get()->last();
-
-
-        
-        
-        // $set = User::select('email')->where('email' , $->get();
-        
-        
+    
         if(Hash::check($fields['password'], $user->password)) {
             
             if($order){
@@ -90,21 +65,24 @@ class AuthController extends Controller
         ]);
             }
             else{
+                $test = $request->bearerToken();
+                $condition = User::where("api_token", $test)->where("id", $user->id)->first();
                 
+            // return response()->json(['message'=> $condition]);
             return response()->json(['message'=> "order not found"]);
-            }
-        
+            
+            }        
     }else{
         return response([
             'error' => 'Invalid email or password'
         ], 401);
     }
 
-    $token = $user->createToken('myapptoken')->plainTextToken;
+    // $token = $user->createToken('myapptoken')->plainTextToken;
 
         $response = [
             'user' => $user,
-            'token' => $token
+            // 'token' => $token
         ];
         return response()->json([$response, 201]);
        
